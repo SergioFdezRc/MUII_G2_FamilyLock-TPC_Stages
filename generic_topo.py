26d2c36058ee7269166666e4077c9a17cefff0ed
Gonzalo <gonzalojh596@gmail.com>
2020-12-11 19:45:00 +0100

    Modifyed a lot of things in topo to make it works

--- generic_topo.py
+++ generic_topo.py
@@ -12,17 +12,17 @@ def createTest(hosts, server, bandwith, test_name):
 	client_filename = "iperfClient" + test_name + ".csv"
 	
 	# From server to client
-	server.cmdPrint("iperf -s -u -b " + str(bandwith) + " -y C >> " + server_filename + " &")
+	server.cmdPrint("iperf -s -u -y C >> " + server_filename + " &")
 
 	for client in hosts:
 		# From client to server
-		client.cmdPrint("iperf -c "+ server.IP()+" -u -b " + str(bandwith) + " -t 10 -i 1 -y C >> " + client_filename + " &")
+		client.cmdPrint("iperf -c "+ server.IP()+" -u -t 10 -i 1 -y C >> " + client_filename + " &")
 
 def createTraffic(hosts, server):
 	#server.cmdPrint("iperf -s -u -y C &")
 	# Test 1 (Bandwith 10 MB/s, Time 10 sec, Interval 0 sec)
 	print("Creating test 1")
-	createTest(hosts, server, 10, "Test1")
+	createTest(hosts, server, "1m", "Test1")
 	'''
 	# Test 2 (Bandwith 100 MB/s, Time 10 sec, Interval 0 sec)
 	print("Creating test 2")
@@ -50,9 +50,9 @@ def createGenericTopo(houses = 1):
 
 	hosts, switches = [], []
 	
-	gs = net.addSwitch('gs0')
+	gs = net.addSwitch('s0')
 	
-	server = net.addHost('server0')
+	server = net.addHost('h0')
 	net.addLink(server,gs)
 
 	for house in range(houses):
